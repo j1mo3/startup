081c6e6f4fe3d00a42b168e67fac7165d4f11434
j1mo3 <jameschem27@gmail.com>
2024-03-22 21:54:59 -0600

    express update

--- database.js
+++ database.js
@@ -8,13 +8,26 @@ const db = client.db('missionaryConnect');
 const postCollection = db.collection('posts');
 const accounts = db.collection('accounts')
 
-(async function testConnection() {
-  await client.connect();
-  await db.command({ ping: 1 });
-})().catch((ex) => {
-  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-  process.exit(1);
-});
+// (async function testConnection() {
+//   await client.connect();
+//   await db.command({ ping: 1 });
+// })().catch((ex) => {
+//   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+//   process.exit(1);
+// });
+
+async function getPosts() {
+  const currentDate = new Date();
+  const startDate = new Date(currentDate);
+  startDate.setDate(startDate.getDate() - 60);
+
+  const query = { date: { $gt: startDate } };
+  const options = {
+    sort: { date: -1 },
+  };
+  const cursor = accounts.find(query, options);
+  return cursor.toArray();
+}
 
 async function checkLogin(username, password){
   //check username and password
@@ -41,18 +54,3 @@ async function addPost(username, date, service_date, text) {
   await collection.insertOne(post);
 }
 
-async function loadPost() {
-  //load posts from database
-  //check username and password
-  //redirect to home screen
-  const currentDate = new Date();
-  const startDate = new Date(currentDate);
-  startDate.setDate(startDate.getDate() - 60);
-
-  const query = { date: { $gt: startDate } };
-  const options = {
-    sort: { score: -1 },
-  };
-  const cursor = accounts.find(query, options);
-  return cursor.toArray();
-}
--- index.js
+++ index.js
@@ -1,44 +1,37 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
-
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
-
 // Router for service endpoints
 var apiRouter = express.Router();
-app.use(`/missionary-connect-api`, apiRouter);
 
-//get service country
-apiRouter.get('/get-country', (_req, res) => {
-  res.send(country)
+app.use(`/api`, apiRouter);
+
+//get
+apiRouter.get('/account', (_req, res) => {
+  res.send(account);
+});
+apiRouter.get('/posts', async (_req, res) => {
+  const posts = await DB.getPosts();
+  res.send(posts);
 });
 
-apiRouter.get('get-area', (_req, res) => {
-  res.send(missionArea)
+// post
+apiRouter.post('/post', (req, res) => {
+  post = addPost(req.body, scores);
+  res.send(post);
 });
-
-//get current posts
-apiRouter.get('/get-posts', (_req, res) => {
-  res.send(posts)
+apiRouter.post('/updateAccount', (req, res) => {
+  account = updateAccount(req.body, scores);
+  res.send(account);
 });
-
-apiRouter.get('/get-login', (_req, res) => {
-  res.send(loginInformation)
-});
-
-
-//make post
-apiRouter.post('/add-post', (req, res) => {
-    scores = updatePosts(req.body, posts);
-    res.send(posts);
-  });
-
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
@@ -48,16 +41,3 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// // updateScores considers a new score for inclusion in the high scores.
-// // The high scores are saved in memory and disappear whenever the service is restarted.
-let posts = [];
-function updatePosts(newPost, posts) {
-  posts.unshift(newPost);
-
-  if (posts.length > 50) {
-    posts.length = 50;
-  }
-
-  return posts;
-}
