4cd1d53335ef99bf05e53f708f4e5dd9ee58015f
j1mo3 <jameschem27@gmail.com>
2024-03-30 17:24:54 -0600

    websocket update

--- public/script.js
+++ public/script.js
@@ -353,30 +353,16 @@ async function displayQuote(country, object) {
 
 async function configureWebSocket() {
     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-    this.socket.onopen = (event) => {
-      this.displayMsg('system', 'game', 'connected');
-    };
-    this.socket.onclose = (event) => {
-      this.displayMsg('system', 'game', 'disconnected');
-    };
-    this.socket.onmessage = async (event) => {
+    socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    
+    socket.onmessage = async (event) => {
       const msg = JSON.parse(await event.data.text());
-      if (msg.type === GameEndEvent) {
-        this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
-      } else if (msg.type === GameStartEvent) {
-        this.displayMsg('player', msg.from, `started a new game`);
-      }
+      //display post
+      displayMsg('player', msg.from, `scored ${msg.value.score}`);
     };
   }
-
-  async function displayMsg(cls, from, msg) {
-    const chatText = document.querySelector('#player-messages');
-    chatText.innerHTML =
-      `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
-  }
-
-  async function broadcastEvent(from, type, value) {
+  //this posts something to websocket which can then be distributed
+async function broadcastEvent(discussion, username, text) {
     const event = {
       from: from,
       type: type,
